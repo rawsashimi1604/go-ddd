106da066bae69902a7c842494bc4e5f01b6138b5
Gavin Loo <looweiren@gmail.com>
2023-08-02 00:00:58 +0800

    added customer aggregate

---
+++ aggregate/customer.go
@@ -0,0 +1,18 @@
+// package aggregate holds our aggregates that combines many entities into a full object
+package aggregate
+
+import (
+	"github.com/rawsashimi1604/ddd-go/entity"
+	"github.com/rawsashimi1604/ddd-go/valueobject"
+)
+
+type Customer struct {
+	// Person is the root entity of customer
+	// which means person.ID is the main identifier for the customer
+
+	// Small case -> external cannot use.
+	// Not accessible directly to the other data. Not accessible from outside.
+	person       *entity.Person
+	products     []*entity.Item
+	transactions []valueobject.Transaction
+}
