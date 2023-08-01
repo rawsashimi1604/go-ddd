32f8ce15aedaafda3ac6501f2030f569b35d0cf7
Gavin Loo <looweiren@gmail.com>
2023-08-02 00:47:57 +0800

    added customer repo

---
+++ domain/customer/memory/memory.go
@@ -0,0 +1,58 @@
+// Package memory is an in-memory implementation of CustomerRepository
+package memory
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/google/uuid"
+	"github.com/rawsashimi1604/go-ddd/aggregate"
+	"github.com/rawsashimi1604/go-ddd/domain/customer"
+)
+
+type MemoryRepository struct {
+	customers map[uuid.UUID]aggregate.Customer
+	sync.Mutex
+}
+
+func New() *MemoryRepository {
+	return &MemoryRepository{
+		customers: make(map[uuid.UUID]aggregate.Customer, 0),
+	}
+}
+
+func (mr *MemoryRepository) Get(id uuid.UUID) (aggregate.Customer, error) {
+	if customer, ok := mr.customers[id]; ok {
+		return customer, nil
+	}
+
+	return aggregate.Customer{}, customer.ErrCustomerNotFound
+}
+
+func (mr *MemoryRepository) Add(c aggregate.Customer) error {
+	if mr.customers == nil {
+		mr.Lock()
+		mr.customers = make(map[uuid.UUID]aggregate.Customer)
+		mr.Unlock()
+	}
+	// Make sure customer is already in repo
+	if _, ok := mr.customers[c.GetID()]; ok {
+		return fmt.Errorf("customer already exists :%w", customer.ErrFailedToAddCustomer)
+	}
+
+	mr.Lock()
+	mr.customers[c.GetID()] = c
+	mr.Unlock()
+	return nil
+}
+
+func (mr *MemoryRepository) Update(c aggregate.Customer) error {
+	if _, ok := mr.customers[c.GetID()]; !ok {
+		return fmt.Errorf("customer does not exist: %w", customer.ErrUpdateCustomer)
+	}
+
+	mr.Lock()
+	mr.customers[c.GetID()] = c
+	mr.Unlock()
+	return nil
+}
